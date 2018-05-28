e7dce753115997948871d9501be5d8a72a599e31
Dmitry Avgustis <avgdima@gmail.com>
2018-05-28 08:53:26 +0200

    security started

--- src/main/java/dmitry/avgustis/prototype/config/SecurityConfig.java
+++ src/main/java/dmitry/avgustis/prototype/config/SecurityConfig.java
@@ -1,15 +1,46 @@
 package dmitry.avgustis.prototype.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig
-//        extends WebSecurityConfigurerAdapter
-{
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomAuthenticationProvider customAuthProvider;
 
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.authorizeRequests().antMatchers("/student/v*/**").permitAll().anyRequest().authenticated().and()
-//                .formLogin()
-//    }
+    @Autowired
+    public SecurityConfig(CustomAuthenticationProvider customAuthProvider) {
+        this.customAuthProvider = customAuthProvider;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        auth.authenticationProvider(customAuthProvider);
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("pass")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .authenticated();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
